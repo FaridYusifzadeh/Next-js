5d391b0da95053c6c7cc04b7e6736da159a3d1cc
FaridYusifzadeh <farid.y@buglance.com>
2021-02-02 12:34:08 +0400

    initial commit

---
+++ components/ArticleList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import articleStyles from '../styles/Article.module.css';
+import ArticleItem from './ArticleItem'
+
+const ArticleList = ({articles}) => {
+    return (
+        <div className={articleStyles.grid}>
+            {articles.map((article, index) => <ArticleItem article={article}/>
+            )}
+        </div>
+    )
+}
+export default ArticleList;
